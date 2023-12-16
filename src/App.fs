e78d2f96005044319c7b78fc8fa10f2212005cd3
taidalog <72747279+taidalog@users.noreply.github.com>
2023-12-16 19:24:48 +0900

    Utilized a mutable variable to hold wakeLockSentinel(?).

--- src/App.fs
+++ src/App.fs
@@ -7,17 +7,16 @@ open Browser.Navigator
 open Fable.Core
 open Fable.Core.JsInterop
 
-let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
-
 [<Emit("setTimeout($0, $1)")>]
 let setTimeout (code: unit -> unit) (delay: int) : int = jsNative
 
-let mutable state: obj = null
+let mutable wakeLock: JS.Promise<obj> = null
 
+let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
 myButton.onclick <- fun _ ->
     myButton.innerText <- sprintf "You clicked on: %s" (DateTime.Now.ToString())
 
-    let wakeLock3 =
+    wakeLock <-
         promise {
             printfn "locking at %s" (DateTime.Now.ToString())
             let! x = navigator?wakeLock?request("screen")
@@ -28,7 +27,7 @@ myButton.onclick <- fun _ ->
 
     setTimeout
         (fun _ ->
-            wakeLock3.``then``(fun x -> 
+            wakeLock.``then``(fun x -> 
                 printfn "releasing at %s" (DateTime.Now.ToString())
                 x?release()
             ) |> ignore)
